87935e86157af4e022786111c432f75e02811811
echos <appjavadev2019@gmail.com>
2024-06-13 11:45:16 -0500

    add content

---
+++ js/classes.js
@@ -0,0 +1,26 @@
+import {createBox} from './selector.js'
+
+export let boxes = []
+
+export class Box {
+    constructor(name, color, h, w) {
+     this.id = Math.random()
+     this.h = h;
+     this.w = w;
+     this.name = name;
+     this.color = color;
+    }
+
+    initBox() {
+        let htmlBox = createBox();
+        //remeber that we are importing createBox() from selectors.js file. Which returns a div element.
+        htmlBox.id = this.id;
+        htmlBox.style.height = `${this.h}px`;
+        htmlBox.style.width = `${this.w}px`;
+        htmlBox.style.margin = '6px';
+        htmlBox.style.backgroundColor = this.color;
+        htmlBox.innerHTML = this.name;
+        boxes.push(htmlBox);
+      }
+
+   }
---
+++ js/main.js
@@ -0,0 +1,32 @@
+import { Box, boxes } from './classes.js';
+import {
+ boxHeight,
+ boxWidth,
+ boxColor,
+ boxName,
+ button,
+ boxStorage
+} from './selectors.js';
+
+function addBox(name, color, h, w) {
+    let box = new Box(name, color, h, w);
+    box.initBox();
+  }
+
+  button.addEventListener('click', () => {
+    addBox(boxName.value, boxColor.value, boxHeight.value, boxWidth.value);
+    boxName.value = '';
+      //this will remove the name value.
+  });
+
+  function update() {
+    for (let i = 0; i < boxes.length; i++) {
+      boxStorage.appendChild(boxes[i]);
+    }
+  }
+  // also update needs to be called when ever we create a new box
+  button.addEventListener('click', () => {
+    addBox(boxName.value, boxColor.value, boxHeight.value, boxWidth.value);
+    update(); // add this to event listener
+    boxName.value = '';
+  });
